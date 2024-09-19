a386415b238ef70d33844524d82600e38757d38d
Irina Klepova <ivklepova23@gmail.com>
2024-09-19 22:11:58 +0400

    описан ViewSet для работы с моделью курса

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from lms.models import Course
+from lms.serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
