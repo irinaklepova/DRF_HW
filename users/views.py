6e5b64e92b3ba7b2247a8e0fb436f5f0fbbecb1a
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 02:13:51 +0400

    добавлен ViewSet для модели платежа

--- users/views.py
+++ users/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """ViewSet для модели пользователя"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    """ViewSet для модели пользователя"""
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
