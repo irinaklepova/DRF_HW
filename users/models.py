9da3322c66448eb26e02c9611e7a1540b564e234
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 01:47:38 +0400

    добавлены поля модели платежа

--- users/models.py
+++ users/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 from config.settings import NULLABLE
+from lms.models import Course, Lesson
 
 
 class User(AbstractUser):
+    """Модель Пользователь"""
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
     phone = models.CharField(max_length=35, **NULLABLE, verbose_name='Телефон')
@@ -20,3 +23,26 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payment(models.Model):
+    """Модель Платеж"""
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'наличные'),
+        ('card', 'перевод на счет'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    payment_day = models.DateTimeField(default=timezone.now, **NULLABLE, verbose_name='Дата оплаты')
+    course_pay = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Оплаченный курс')
+    lesson_pay = models.ForeignKey(Lesson, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Оплаченный урок')
+    payment_amount = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=25, choices=PAYMENT_METHOD_CHOICES, **NULLABLE,
+                                      verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return (f'{self.user}: {self.payment_day}, {self.payment_amount}, '
+                f'{self.course_pay if self.course_pay else self.lesson_pay}')
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
