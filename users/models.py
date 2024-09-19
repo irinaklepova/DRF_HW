7027c4998eba35feb6e996732cffa4c8c8cd7ef2
Irina Klepova <ivklepova23@gmail.com>
2024-09-19 16:48:23 +0400

    добавлены поля в модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from config.settings import NULLABLE
+
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, **NULLABLE, verbose_name='Телефон')
+    city = models.CharField(max_length=100, **NULLABLE, verbose_name='Город')
+    avatar = models.ImageField(upload_to='users/', **NULLABLE, verbose_name='Аватар')
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
