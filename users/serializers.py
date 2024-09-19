50f24ea1fee0d4f04510d6970c84e1137dccf15d
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 01:58:34 +0400

    добавлен сериализатор для модели платежа

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import User
+from users.models import User, Payment
 
 
 class UserSerializer(ModelSerializer):
@@ -9,3 +9,11 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'phone', 'password', 'is_active', 'is_staff', 'is_superuser',)
+
+
+class PaymentSerializer(ModelSerializer):
+    """Сериализатор для модели платежа"""
+
+    class Meta:
+        model = Payment
+        fields = "__all__"
