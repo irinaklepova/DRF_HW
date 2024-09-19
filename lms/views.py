f42386392c19bc5c5720120649356c483e917e0d
Irina Klepova <ivklepova23@gmail.com>
2024-09-19 23:15:03 +0400

    описан ViewSet для работы с моделью курса

--- lms/views.py
+++ lms/views.py
@@ -6,6 +6,7 @@ from lms.serializers import CourseSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
+    """ViewSet для модели курса"""
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
