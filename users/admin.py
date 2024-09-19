a5648f7d42fc1e9dd211545a0e48caf463ebfd4c
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 02:30:59 +0400

    добавлена админка для модели пользователя и платежа

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Payment, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'id',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'course', 'lesson', 'payment_day', 'payment_amount', 'payment_method',)
