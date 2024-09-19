094569c118fc9b453c2161759d3cbcb4a0ab7f9d
Irina Klepova <ivklepova23@gmail.com>
2024-09-19 17:00:11 +0400

    добавлено строковое представление и мета в модели курса

--- lms/models.py
+++ lms/models.py
@@ -6,3 +6,11 @@ class Course(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
     preview = models.ImageField(upload_to='lms/', **NULLABLE, verbose_name='Превью')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
