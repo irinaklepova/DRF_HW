b8e16c05790cdc5ffa727ad5f8a89b4391eddaa2
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 00:02:06 +0400

    описана маршрутизация для Generic-классов модели урока

--- lms/urls.py
+++ lms/urls.py
@@ -1,7 +1,8 @@
 from lms.apps import LmsConfig
 from rest_framework.routers import DefaultRouter
-
-from lms.views import CourseViewSet
+from django.urls import path
+from lms.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
+    LessonDestroyAPIView
 
 app_name = LmsConfig.name
 
@@ -9,5 +10,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
-] + router.urls
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+                  path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+                  path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+                  path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+                  path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+              ] + router.urls
