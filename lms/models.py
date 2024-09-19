f0c29e84148dab8a358011063d349f358c919823
Irina Klepova <ivklepova23@gmail.com>
2024-09-19 17:06:47 +0400

    добавлены поля, строковое представление и мета в модели урока

--- lms/models.py
+++ lms/models.py
@@ -8,9 +8,23 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='lms/', **NULLABLE, verbose_name='Превью')
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
 
+
+class Lesson(models.Model):
+    name_les = models.CharField(max_length=200, verbose_name='Название урока')
+    description_les = models.TextField(**NULLABLE, verbose_name='Описание урока')
+    preview_les = models.ImageField(upload_to='lms/', **NULLABLE, verbose_name='Превью урока')
+    video_link = models.URLField(max_length=200, **NULLABLE, verbose_name='Ссылка на видео')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Курс')
+
+    def __str__(self):
+        return f'{self.name_les}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
