d6355817d7f5cf3a4cd45146ab56345b5cfdc4ce
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 23:19:48 +0400

    реализован CRUD для пользователей

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializer
@@ -25,4 +25,28 @@ class PaymentViewSet(viewsets.ModelViewSet):
 
 class UsersCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDetailPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDeleteAPIView(DestroyAPIView):
     queryset = User.objects.all()
