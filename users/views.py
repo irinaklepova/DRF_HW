f4211ad01f760b0f125d35f2ca829a00eb0c8c09
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 15:07:18 +0400

    настроена фильтрация для эндпоинта вывода списка платежей по курсу/уроку, способу оплаты, сортировка по дате оплаты

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializer
@@ -15,3 +16,7 @@ class PaymentViewSet(viewsets.ModelViewSet):
     """ViewSet для модели пользователя"""
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course_pay', 'lesson_pay', 'payment_method',)
+    ordering_fields = ('payment_day',)
