5e6b55c59bec414987e074fc41d06c3d2936c523
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 15:08:47 +0400

    удалена админка

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
 
-from users.models import Payment, User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email',)
-
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'course', 'lesson', 'payment_day', 'payment_amount', 'payment_method',)
+# Register your models here.
