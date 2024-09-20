62ab2897daec70842085523860e20c44dcca08a6
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 23:32:12 +0400

    описана маршрутизация для контроллеров модели пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,23 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserViewSet, PaymentViewSet
+from users.views import PaymentViewSet, UsersCreateAPIView, UserListView, UserDetailPIView, \
+    UserUpdateAPIView, UserDeleteAPIView
 
 app_name = UsersConfig.name
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
+# router.register(r'users', UserViewSet, basename='users')
 router.register(r'payments', PaymentViewSet, basename='payments')
 urlpatterns = [
+                  path('register/', UsersCreateAPIView.as_view(), name='register'),
+                  path('users_list/', UserListView.as_view(), name='users_list'),
+                  path('users/<int:pk>/', UserDetailPIView.as_view(), name='users_detail'),
+                  path('users/update/<int:pk>/', UserUpdateAPIView.as_view(), name='users_update'),
+                  path('users/delete/<int:pk>/', UserDeleteAPIView.as_view(), name='users_delete'),
 
-] + router.urls
+                  path('token/', TokenObtainPairView.as_view(), name='token'),
+                  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+              ] + router.urls
