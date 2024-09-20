399e131a402a7ba22101101e355ecb32dbc92f58
Irina Klepova <ivklepova23@gmail.com>
2024-09-20 15:49:20 +0400

    расширен сериализатор для вывода истории платежей для профиля пользователя

--- users/serializers.py
+++ users/serializers.py
@@ -3,17 +3,18 @@ from rest_framework.serializers import ModelSerializer
 from users.models import User, Payment
 
 
-class UserSerializer(ModelSerializer):
-    """Сериализатор для пользователя"""
-
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'phone', 'password', 'is_active', 'is_staff', 'is_superuser',)
-
-
 class PaymentSerializer(ModelSerializer):
     """Сериализатор для модели платежа"""
 
     class Meta:
         model = Payment
+        fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    """Сериализатор для пользователя"""
+    payment_history = PaymentSerializer(source='user', many=True, read_only=True)
+
+    class Meta:
+        model = User
         fields = "__all__"
