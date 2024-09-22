2f8ea21e7f74992a954316f702544520fb55305a
Irina Klepova <ivklepova23@gmail.com>
2024-09-22 18:12:11 +0400

    добавлен класс-валидатор для проверки ссылки в курсе и уроке

---
+++ lms/validators.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ValidationError
+
+
+class UrlValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        reg = 'https://www.youtube.com/'
+        tmp_value = dict(value).get(self.field)
+        if tmp_value is not None:
+            if reg not in tmp_value:
+                raise ValidationError('Недопустимый URL-адрес')
